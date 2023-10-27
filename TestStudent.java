a7758a1542a4206d7866b370e0a1b68a33122bda
ShaileshPatil2000 <92880083+ShaileshPatil2000@users.noreply.github.com>
2023-10-27 04:15:15 -0700

    Add files via upload

---
+++ TestStudent.java
@@ -0,0 +1,11 @@
+
+public class TestStudent {
+
+	public static void main(String[] args) {
+			Student s=new Student(1,"Shailesh",80);
+			Student s1=new Student(2,"Rutik",70);
+			s.displayAll();
+			s1.displayAll();
+	}
+
+}
