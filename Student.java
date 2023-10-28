6e824ca780cdc3a558c03fcce5f57c94b5df58ed
ShaileshPatil2000 <92880083+ShaileshPatil2000@users.noreply.github.com>
2023-10-28 00:51:03 -0700

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,64 +1,33 @@
 
-public class Student 
+public class Student extends Person
 {
-	private int id;
-	private String name;
-	private int marks;
-	
-	//Default Constructor
-	public Student()
-	{
-		this.id=id;
-		this.name=name;
-		this.marks=marks;
-		
+	private int sid;
+	private String sname;
+	public Student() {
+		System.out.println("in student default constructor");
+	}
+	public Student(int pid, String pname,int sid, String sname) {
+		super(pid, pname);
+		System.out.println("in student parameterized constructor");
+		this.sid = sid;
+		this.sname = sname;
 	}
 	
-	//Parameterized Constructor
-	public Student(int sid, String sname,int smark)
-	{
-		id=sid;
-		name=sname;
-		marks=smark;
+	public int getSid() {
+		return sid;
 	}
-	
-	//Mutator(Setter method)
-	
-	public void setSid(int sid)
-	{
-		this.id=sid;
+	public void setSid(int sid) {
+		this.sid = sid;
 	}
-	
-	public void setSname(String sname)
-	{
-		this.name=sname;
+	public String getSname() {
+		return sname;
 	}
-	
-	public void setSmarks(int smark)
-	{
-		this.marks=smark;
+	public void setSname(String sname) {
+		this.sname = sname;
 	}
-	
-	//Accessor (getter method)
-	
-	public int getSid()
-	{
-		return id;
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + "]";
 	}
-	public String getSname()
-	{
-		return name;
-	}
-	public int getMarks()
-	{
-		return marks;
-	}
-	
-	public void displayAll()
-	{
-		System.out.println("Sid: "+id);
-		System.out.println("Sname: "+name);
-		System.out.println("Marks: "+marks);
-		System.out.println();
-	}
+
 }
