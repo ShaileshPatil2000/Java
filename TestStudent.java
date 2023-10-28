3fb650354c45f888c89533b2c357157977ea9ccc
ShaileshPatil2000 <92880083+ShaileshPatil2000@users.noreply.github.com>
2023-10-28 00:51:19 -0700

    Add files via upload

--- TestStudent.java
+++ TestStudent.java
@@ -1,11 +1,11 @@
 
-public class TestStudent {
+public class TestStudent 
+{
 
-	public static void main(String[] args) {
-			Student s=new Student(1,"Shailesh",80);
-			Student s1=new Student(2,"Rutik",70);
-			s.displayAll();
-			s1.displayAll();
+	public static void main(String[] args) 
+	{
+		Student s=new Student(1,"Anil",11,"Rutik");
+		System.out.println(s);
 	}
 
 }
